fc6a7617ed56ee2438a70c10875f3935917867a5
Do Trung Kien <trungkien.keio@gmail.com>
2019-06-11 14:48:49 +0700

    update kuber & contract config

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -2,8 +2,8 @@ const { readFileSync } = require('fs');
 const LoomTruffleProvider = require('loom-truffle-provider');
 
 const chainId = 'default';
-const writeUrl = 'http://192.168.99.100:46658/rpc';
-const readUrl = 'http://192.168.99.100:46658/query';
+const writeUrl = 'http://35.247.181.254:46658/rpc';
+const readUrl = 'http://35.247.181.254:46658/query';
 
 const privateKey = readFileSync('./priv', 'utf-8');
 const localLoomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
